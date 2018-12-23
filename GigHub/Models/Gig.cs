f6706faffd2ea4767e13fdd2ec70c702cff6d5d7
Kert Kullerkann <KertKullerkann@hotmail.com>
2018-12-23 19:50:53 +0200

    Changes in Gigs and Generes Database mifrations

--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHub.Models
 {
     public class Gig
     {
         public int Id { get; set; }
+
+        [Required] //Not null
         public ApplicationUser Artist { get; set; }
+
         public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]  //Change length in SQL
         public string Venue { get; set; }
+
+        [Required]
         public Genre Genre { get; set; }
     }
 }
