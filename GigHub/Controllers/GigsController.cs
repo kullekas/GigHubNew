677e45f93f1a510cfccd25770bfaf646fc23b954
Kert Kullerkann <KertKullerkann@hotmail.com>
2018-12-24 09:05:39 +0200

    Added a form with asp.net and saved data from form to database.

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,7 +1,9 @@
-﻿using System.Linq;
-using System.Web.Mvc;
-using GigHub.Models;
+﻿using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
@@ -14,9 +16,9 @@ namespace GigHub.Controllers
         {
             _context = new ApplicationDbContext();
         }
-    
 
-        // GET: Gigs
+
+        [Authorize]
         public ActionResult Create()
         {
             var viewmodel = new GigFormViewModel
@@ -27,6 +29,43 @@ namespace GigHub.Controllers
             };
 
             return View(viewmodel);
+        }
+
+        //Form submit Action button.
+        [Authorize]
+        [HttpPost]  //On Post
+        public ActionResult Create(GigFormViewModel viewModel)
+
+
+        {
+            //Makeing varaible, cause SQL sentence cant read C# language. 
+            var artistId = User.Identity.GetUserId();
+
+            //Returns Appliction user object, that can be associated eith this gig.
+            var artist = _context.Users.Single(u => u.Id == artistId);
+
+            //Select Genre from Db.
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+
+            var gig = new Gig
+            {
+                Artist = artist,
+
+                //But two stings from date in viewmodel together to one Datetime object.
+                DateTime = DateTime.Parse($"{viewModel.Date} {viewModel.Time}"),
+                Genre = genre,
+                Venue = viewModel.Venue
+            };
+
+            //We have a gig object and we need to hava it in the context.
+            _context.Gigs.Add(gig);
+
+            //Entity framework will make a SQL statement and save it to Db.
+            _context.SaveChanges();
+
+            //Post return. We are coing to redirect the user back to homepage.
+            return RedirectToAction("Index", "Home");
+
         }
     }
 }
